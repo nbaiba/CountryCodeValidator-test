9ec62e2a44bef5fcc3943036d8fd0bfff617bba4
nbaiba <43407465+nbaiba@users.noreply.github.com>
2022-05-23 22:23:51 +0300

    added comment

--- index.test.js
+++ index.test.js
@@ -8,10 +8,24 @@ class ValidationSuccess {
 
 class ValidationError {
   constructor(value) {
-    this.error = false;
+    this.error = true;
   }
 }
 
+/* 
+  class CountryCodeValidator checks if given value:
+    1.  if(!this.value), because of logical NOT operator, would return true
+    if value given is falsey ('', 0, null, undefined, NaN, false) - these
+    values should return true so the new ValidationSuccess() will be returned.
+
+    2.(this.value && this.value.length === 0) checks nothing, because value.length === 0
+    can be returned only to empty string, but that has been already checked in if statement above
+
+    3. Regex expression /^[A-Za-z]{3}$/ checks if in given value meet these requirements
+      1. uppercase (A-Z) and/or
+      2. lowercase (a-z) letters and
+      3. 3 letters allowed 
+*/
 class CountryCodeValidator {
   constructor(value) {
     this.value = value;
@@ -35,7 +49,7 @@ describe("CountryCodeValidator", () => {
   });
 
   it("undefined value?", () => {
-    expect(new CountryCodeValidator(undefined).validate()).toEqual(
+    expect(new CountryCodeValidator(false).validate()).toEqual(
       new ValidationSuccess()
     );
   });
